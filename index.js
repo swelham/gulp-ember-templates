790415efa5d9b1496e2816c57e229fff442c8ebd
stuart.welham@gmail.com <stuart.welham@gmail.com>
2014-05-19 15:03:01 +0100

    basic implementation

---
+++ index.js
@@ -0,0 +1,29 @@
+var compiler = require('ember-template-compiler');
+var through = require('through2');
+var gutil = require('gulp-util');
+var PliginError = gutil.PliginError;
+
+const PLUGIN_NAME = 'gulp-ember-templates';
+const TEMPLATE_PREFIX = 'Ember.TEMPLATES["application"] = Ember.Handlebars.template(';
+const TEMPLATE_SUFFIX = ');';
+
+function compile() {
+  var stream = through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      return cb();
+    }
+
+    var compilerOutput = compiler.precompile(file.contents.toString());
+
+    if (file.isBuffer()) {
+      file.contents = new Buffer(TEMPLATE_PREFIX + compilerOutput.toString() + TEMPLATE_SUFFIX);
+    }
+
+    this.push(file);
+    return cb();
+  });
+
+  return stream;
+}
+
+module.exports = compile;
